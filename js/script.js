8fbfb12f9c26f9174464277d1036706d9d3b49f1
ayesh156 <ayeshchathuranga531@gmail.com>
2022-02-13 10:45:34 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,81 @@
+
+/*-- Navigation Bar--*/
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+/*-- theme toggler --*/
+
+let themeToggler = document.querySelector('.theme-toggler');
+let toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.onclick = () => {
+  themeToggler.classList.toggle('active');
+}
+
+/*-- remove action  --*/
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    themeToggler.classList.remove('active');
+}
+
+document.querySelectorAll('.theme-toggler .theme-btn').forEach(btn =>{
+  
+  btn.onclick = () =>{
+    let color = btn.style.background;
+    document.querySelector(':root').style.setProperty('--main-color', color);
+  }
+
+});
+
+/*-- Home Slider --*/
+
+var swiper = new Swiper(".home-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+    loop:true,
+    autoplay:{
+        delay: 3000,
+        disableOnInteraction: false,
+    }
+});
+
+/*-- Review Slider --*/
+
+var swiper = new Swiper(".review-slider", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop:true,
+    spaceBetween: 10,
+    breakpoints: {
+      0: {
+            slidesPerView: 1,
+      },
+      700: {
+            slidesPerView: 2,
+      },
+      1050: {
+            slidesPerView: 3,
+      },    
+    },
+    autoplay:{
+      delay: 5000,
+      disableOnInteraction:false,
+  }
+});
